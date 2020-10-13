17c7a136d2c8751a1b2c2ab1ca1c96c29266be0c
Franki Hackett <hackett.franki@gmail.com>
2020-10-13 12:06:20 +0200

    Pivoted my data!

--- missing data functions.R
+++ missing data functions.R
@@ -1,10 +1,13 @@
 install.packages("naniar")
 install.packages("heatmaply")
+install.packages("reshape")
 
 library(naniar)
 library(ggplot2)
 library(heatmaply)
 library(dplyr)
+library(reshape)
+library(tidyr)
 
 
 
@@ -25,7 +28,7 @@ Missing_data$corporation_taxes_eur <- NULL
 Missing_data$pbt_eur <- NULL
 Missing_data$revenue_eur <- NULL
 
-#Preparing data to go into heatmap
+#Preparing data to go into heatmap - pos v negative
 
 Missing_data$corporation_taxes_original_currency[!is.na(Missing_data$corporation_taxes_original_currency)] <- 1
 Missing_data$revenue[!is.na(Missing_data$revenue)] <- 1
@@ -37,7 +40,31 @@ Missing_data$revenue[is.na(Missing_data$revenue)] <- 0
 Missing_data$pbt[is.na(Missing_data$pbt)] <- 0
 Missing_data$number_of_employees[is.na(Missing_data$number_of_employees)] <- 0
 
+
+#Grouping data
+
+Missing_data$company_country <- paste(Missing_data$year, Missing_data$company, Missing_data$country, sep = " - ")
+Missing_data$company <-NULL
+Missing_data$country <-NULL
+Missing_data$year <-NULL
+
+Pivot_missing <- gather(Missing_data, key = value_type, value = "value", 
+                        corporation_taxes_original_currency, 
+                        revenue,
+                        pbt,
+                        number_of_employees, na.rm = FALSE)
+
+
+
 #using ggplot2 to build heatmap
 
-ggplot(Missing_data, aes(x= c() y = y))+
+ggplot(Missing_2018, aes(x= year, y = company,
+                         fill = corporation_taxes_original_currency)) +
   geom_tile()
+
+
+x <- LETTERS[1:20]
+y <- paste0("var", seq(1,20))
+data <- expand.grid(X=x, Y=y)
+data$Z <- runif(400, 0, 5)
+  
