c0d9bfc9d47f7289406f65aaa88a99eceac1f28f
Franki Hackett <hackett.franki@gmail.com>
2020-10-20 10:20:07 +0200

    Finishing normalisation functions

--- normalisatiion functions.R
+++ normalisatiion functions.R
@@ -32,7 +32,7 @@ Efctve_rate <- Efctve_rate[effective_rate > 0]
 
 Company_revs <- Tax_aligned %>%
   group_by(company, year) %>%
-  summarize(global_rev = sum(revenue_eur, na.rm = T)) #for reasons I don't understand, this works...
+  summarize(global_rev = sum(revenue_eur, na.rm = T)) 
 
 Company_peeps <- Tax_aligned %>%
   group_by(company, year) %>%
@@ -83,3 +83,66 @@ Normal_rev <- merge(Normal_rev, real_rates, all.x = T, by = c("country", "year")
 
 Normal_rev <- Normal_rev %>%
   mutate(normal_real_tax = norm_rev * rate / 100)
+
+
+################################################ Profit normalisation ##########################################
+
+### Creating global profit margin per company
+
+Company_profs <- Tax_aligned %>%
+  group_by(company, year) %>% 
+  summarize(comp_rev = sum(revenue_eur, na.rm = T), comp_profit = sum(pbt_eur, na.rm = T))
+
+Company_profs <- mutate(Company_profs, prof_marg = comp_rev / comp_profit)
+
+Company_profs <- Company_profs[prof_marg > 0]
+
+
+
+### Creating normalised profits
+# This shows how much profit  companies would have earned if profits were earned at the same margin across the world
+
+Normal_prof <- Tax_aligned[, c("company", "country", "year", 
+                              "tax_for_the_year_eur", "revenue_eur", "pbt_eur")]
+
+Normal_prof <- merge(Normal_prof, Company_profs, all.x = T, by = c("company", "year"))
+
+Normal_prof <- Normal_prof %>%
+  mutate(norm_prof = revenue_eur * prof_marg)
+
+Normal_prof <- Normal_prof[norm_prof > 0] # this removes rows where there is no data
+
+
+#Tax on normalised profit - effective rate
+Normal_prof <- merge(Normal_prof, Efctve_rate, all.x = T, by = c("country", "year"))
+
+Normal_prof <- Normal_prof %>%
+  mutate(normal_tax = norm_prof * effective_rate)
+
+
+#### Tax on normalised profit - real rate
+# This uses the real tax rate to tax normalised profit
+
+real_rates <- fread("C:/Users/Admin/Documents/EUI/Corporate-Tax-Rates-Data-1980-2019.csv", sep = ",", 
+                    encoding = "UTF-8", integer64= "numeric")
+
+real_rates[is.na(real_rates)] <- 0
+
+real_rates$rate <- as.numeric(real_rates$rate)
+
+Normal_prof <- merge(Normal_prof, real_rates, all.x = T, by = c("country", "year"))
+
+Normal_prof <- Normal_prof %>%
+  mutate(normal_real_tax = norm_prof * rate / 100)
+
+
+######################################### Tax normalisation ######################################################
+
+Normal_tax <-Tax_aligned[, c("company", "country", "year", 
+                             "tax_for_the_year_eur", "revenue_eur", "pbt_eur")]
+
+Normal_tax <- merge(Normal_tax, real_rates, all.x = T, by = c("country", "year"))
+
+Normal_tax <- Normal_tax %>%
+  mutate(real_tax_eur = pbt_eur * rate / 100)
+
