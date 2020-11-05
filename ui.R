949173bbb7f1ab211c48457888d1c3569790a751
Franki Hackett <hackett.franki@gmail.com>
2020-11-05 17:18:11 +0100

    UI UI UI delilah

--- ui.R
+++ ui.R
@@ -1,47 +1,68 @@
 library(shiny)
 library(shinythemes)
+library(shinyWidgets)
+library(extrafont)
 
+font_import()
 
 ui <- navbarPage("Website",
-                 tabPanel("Introduction", 
-                          style = "background-color: #dbd1d2;",
+                tabPanel("Missing data", 
                           fluidPage(
+                            setBackgroundColor(color = "#05234F"),
                             fluidRow(
-                              column(8, "Intro text you know", 
-                                     style = "background-color: #05234f;", 
+                              column(8, "Missing data graphic",
                                      style = "color: #dbd1d2;"),
-                              column(4, "sidey sidey side",
-                                     style = "background-color: #dbd1d2;",
-                                     style = "color: #05234f;"))
-                          )
-                 ),
-                 tabPanel("Missing data", 
-                          fluidPage(
-                            fluidRow(
-                              column(8, "Missing data graphic"),
-                              column(4, "sidey sidey side"))
+                              column(4, "sidey sidey side", 
+                                     style = "background-color: #dbd1d2;"))
                           )
                  ),
                  tabPanel("Maps",
                           fluidPage(
+                            setBackgroundColor(color = "#05234F"),
                             fluidRow(
-                              column(8, "Maps go here"),
-                              column(4, "sidey sidey side"))
+                              column(8, "Maps go here",
+                                     style = "color: #dbd1d2;"),
+                              column(4, "sidey sidey side",
+                                     style = "background-color: #dbd1d2;"))
                           )
                  ),
                  tabPanel("Normalization",
                           fluidPage(
+                            setBackgroundColor(color = "#05234F"),
                             fluidRow(
-                              column(8, "Normalism"),
-                              column(4, "sidey sidey side"))
+                              column(8, "Normalism",
+                                     style = "color: #dbd1d2;"),
+                              column(4, "sidey sidey side",
+                                     style = "background-color: #dbd1d2;"))
                           )
                  ),
-                 tags$style(type = 'text/css', 
-                            '.navbar { background-color: #79A9D7;}',
-                            '.navbar-default .navbar-brand{color: #05234F;}',
-                            '.tab-panel{ background-color: #05234F; color: #DBD1D2;}'
-                 )
+                 
+                 tags$style(HTML(" 
+                    .navbar-default .navbar-brand {color: #05234F;}
+                    .navbar-default .navbar-brand:hover {color: #05234F;}
+                    .navbar { background-color: #79A9D7;}
+                    .navbar-default .navbar-nav > li > a {color:#05234F;}
+                    .navbar-default .navbar-nav > .active > a,
+                    .navbar-default .navbar-nav > .active > a:focus,
+                    .navbar-default .navbar-nav > .active > a:hover {color: #E99688;background-color: #05234F;}
+                    .navbar-default .navbar-nav > li > a:hover {color: #C8470E;}")
+                   ),
+                
+                tabPanel("About the data", 
+                         # style = "background-color: #dbd1d2;",
+                         fluidPage(
+                           setBackgroundColor(color = "#05234F"),
+                           fluidRow(
+                             column(1),
+                             column(10, about_data, 
+                                    style = "background-color: #05234f;", 
+                                    style = "color: #dbd1d2;"),
+                             column(1)
+                           )
+                         )
+                )
 )
+
 
 server <- function(input, output, 
                    session){
---
+++ website_text.R
@@ -0,0 +1,23 @@
+##### This page holds all the text required, to make text editing easier
+
+
+
+######Intro text
+
+
+intro_text <- "Banks headquartered in Europe have to publish financial information on their revenues profit before tax, staff numbers, and corporation taxes paid in each country worldwide. The aim of this publication is to prevent and allow detection of tax avoidance.
+
+This website brings together data from the top xx European banks 2017-2020, and gives you the tools to visualize that data, hopefully to help you understand it.
+
+This data comes with health warnings. The standards for data publication [link] are fairly broad, which means that different companies prepare the data in different ways, cover different time periods, and use different approaches to deciding where some activities take place. You can find a fuller list of problems with the data elsewhere on this site.
+
+The first problem with the data is what is available. So let’s start with that…"
+
+
+
+
+###### About the data text
+about_data <- "Here is some stuff about how crappy this data is
+
+
+I wonder if returns are accepted here?"
